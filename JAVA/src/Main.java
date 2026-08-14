0b1fbec9afad1823d2ac44af5094268f1b16c3ce
luciasara2304-ui <luciasara2304@gmail.com>
2026-08-14 09:03:36 -0500

    Creación taller en blanco

---
+++ JAVA/src/Main.java
@@ -0,0 +1,11 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        int [] miArray = {70,12,19,45,11,33,26};
+        MisAlgoritmos misAlgoritmos = new MisAlgoritmos();
+
+        System.out.println(misAlgoritmos.binarySearch(miArray, 19));
+
+        }
+    }
